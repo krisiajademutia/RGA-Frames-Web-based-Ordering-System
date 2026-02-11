981a44e6d6008453a9e82fefc3dd8cc8721cb21e
Krisia Jade Mutia <mutiakrisiajade@gmail.com>
2026-02-11 12:37:20 +0800

    edit

--- config/config.example.php
+++ config/config.example.php
@@ -1,10 +1,4 @@
 <?php
-// Database & Email Configuration
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'rga_frames_db');
-
 // Brevo SMTP Settings
 define('SMTP_HOST', '');
 define('SMTP_USER', '');
@@ -12,3 +6,4 @@ define('SMTP_PASS', '');
 define('SMTP_PORT', 587);
 define('FROM_EMAIL', '');
 define('FROM_NAME', 'RGA Frames');
+?>
