17d2a18601f890618904281ef4a57ad392412d2f
May <p.baldeconza.cs@gmail.com>
2026-03-03 17:30:33 +0800

    my local update to printing page

--- customer/customer_shop_printing.php
+++ customer/customer_shop_printing.php
@@ -1,2 +1,21 @@
- <?php include 'includes/customer_header.php'; ?>
-<!-- KAREENA --->
+<?php include '../includes/customer_header.php'; ?>
+<!-- MAY--->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <br>
+    <br>
+
+    <br>
+
+    <br>
+    <br>
+
+    <p>Hello World</p>
+ </body>
+ </html>
