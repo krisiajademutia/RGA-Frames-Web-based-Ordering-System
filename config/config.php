21c404ec6dde99ffff69d66fa36f5ffc685c19c9
Krisia Jade Mutia <mutiakrisiajade@gmail.com>
2026-05-20 10:41:08 +0800

    updated hardcoded SMTP in config
    Removed comment about fetching the password securely.

--- config/config.php
+++ config/config.php
@@ -2,7 +2,6 @@
 // Brevo SMTP Settings
 define('SMTP_HOST', 'smtp-relay.brevo.com');
 define('SMTP_USER', 'a158aa001@smtp-brevo.com');
-// Securely fetch the password from Render's environment variables
 define('SMTP_PASS', getenv('SMTP_PASS')); 
 define('SMTP_PORT', 587);
 define('FROM_EMAIL', 'mutiakrisiaj@gmail.com');
